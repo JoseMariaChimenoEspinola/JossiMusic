3dddf4226e8fb504f6657cc2a42e7a4a6f82d932
root <JoseMariaChimenoEspinola>
2020-05-29 18:15:07 +0000

    Saving backend

--- backend/src/init.py
+++ backend/src/init.py
@@ -38,6 +38,7 @@ def getUser(usuario,password):
 
 @app.route('/api/register', methods=['POST'])
 def setNewUser():
+    validation = True
     check = db.find_one({'usuario': request.json['usuario'], 'email':request.json['email']})
     if(check == None):
         db.insert({
@@ -46,13 +47,20 @@ def setNewUser():
             'fecha': request.json['fecha'],
             'contra': request.json['contra'],
             'genero': request.json['genero'],
+            'foto': request.json['urlPhoto']
         })
-        #newUser(request.json['email'])
-
-    sys.stderr.write('log mgs'+str(check))
+        validation = True
+    else:
+        validation = False
     
-    return check['usuario']
+    return str(validation)
 
+
+@app.route('/api/getAvatarPhoto/<artista>', methods=['GET'])
+def getAvatarPhoto(artista):
+
+    gen = db.find_one({'usuario': artista}, {"_id": 0, "foto": 1})
+    return jsonify(gen)
 
 # Upload songs and files
 
@@ -73,8 +81,63 @@ def uploadSong():
     
     return 'ok'
 
-#get music users
 
+@app.route('/api/getCancionBuscador/<info>', methods=['GET'])
+def getCancionBuscador(info):
+
+    songs = []
+    search = dbfiles.find({'titulo': {"$regex": '^'+info}})
+
+    for doc in search:
+        songs.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'titulo': doc['titulo'],
+            'artista': doc['artista'],
+            'genero': doc['genero'],
+            'dircancion': doc['dircancion'],
+            'dircaratula': doc['dircaratula'],
+            'fecha': doc['fecha'],
+            'reproducciones': doc['reproducciones'],
+            'likes': doc['likes']
+        })
+
+    return jsonify(songs)
+
+@app.route('/api/getArtistaBuscador/<info>', methods=['GET'])
+def getArtistaBuscador(info):
+
+    arts = []
+    search = db.find({'usuario': {"$regex": '^'+info}})
+
+    for doc in search:
+        arts.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'usuario': doc['usuario'],
+            'foto': doc['foto']
+        })
+
+    return jsonify(arts)
+
+# OBTENER CANCION PO ID
+@app.route('/api/getCancionPorId/<id>', methods=['GET'])
+def getCancionPorId(id):
+
+    song = dbfiles.find_one({'_id': ObjectId(id)}, {
+        '_id': 1,
+        'titulo': 1,
+        'artista': 1,
+        'genero': 1,
+        'dircancion': 1,
+        'dircaratula': 1,
+        'fecha': 1,
+        'reproducciones': 1,
+        'likes': 1
+    })
+
+
+    return jsonify(song)
+
+#get music users
 @app.route('/api/getMusic/<artista>', methods=['GET'])
 def getUserSong(artista):
 
@@ -111,11 +174,43 @@ def checkPassword(usuario, password):
     return str(validation)
 
 
-@app.route('/api/changePassword/<usuario>/<password>', methods=['PUT'])
-def changePassword(usuario,password):
-    db.update_one({'usuario': usuario}, {'$set': {'contra': password}})
+@app.route('/api/changePassword/<usuario>', methods=['PUT'])
+def changePassword(usuario):
+
+    db.update_one({'usuario': usuario}, {'$set': {"contra": request.json['newPassword2']}})
 
     return 'ok'
+
+
+#Update style music conf
+
+
+@app.route('/api/changeStyle/', methods=['PUT'])
+def uploadGen():
+    validation = True
+    
+    if request.json['genero'] != '':
+        db.update_one({'usuario': request.json['usuario']},{'$set': {"genero": request.json['genero']}})
+        validation = True
+    else:
+        validation = False
+
+    sys.stderr.write(str(validation))
+
+    return str(validation)
+
+
+@app.route('/api/checkStyleMusic/<usuario>', methods=['GET'])
+def checkGen(usuario):
+    
+    gen = db.find_one({'usuario':usuario}, {"_id":0,"genero":1})
+    
+    sys.stderr.write(str(gen))
+
+    return jsonify(gen)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
